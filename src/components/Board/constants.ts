5f7f0d6e926c0237ada9e5e97747b62ec5cbd45a
b0006 <qsefthuken@gmail.com>
2021-10-18 15:01:04 +0700

    fix(board): reverse arrays

--- src/components/Board/constants.ts
+++ src/components/Board/constants.ts
@@ -1,5 +1,7 @@
-export const VERTICAL_SYMBOLS = ['1', '2', '3', '4', '5', '6', '7', '8'];
-export const VERTICAL_SYMBOLS_REVERSE = VERTICAL_SYMBOLS.reverse();
+const VERTICAL_SYMBOLS = ['1', '2', '3', '4', '5', '6', '7', '8'];
+const VERTICAL_SYMBOLS_REVERSE = [...VERTICAL_SYMBOLS].reverse();
 
-export const HORIZONTAL_SYMBOLS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-export const HORIZONTAL_SYMBOLS_REVERSE = HORIZONTAL_SYMBOLS.reverse();
+const HORIZONTAL_SYMBOLS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+const HORIZONTAL_SYMBOLS_REVERSE = [...HORIZONTAL_SYMBOLS].reverse();
+
+export { VERTICAL_SYMBOLS, VERTICAL_SYMBOLS_REVERSE, HORIZONTAL_SYMBOLS, HORIZONTAL_SYMBOLS_REVERSE };
