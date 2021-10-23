cd80cecaa68c00acc29dafaaf840e4fb02974974
b0006 <qsefthuken@gmail.com>
2021-10-23 22:10:59 +0700

    ref(api): env

--- src/agent/axios.ts
+++ src/agent/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const HTTP = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:4000',
   withCredentials: true,
 });
 
