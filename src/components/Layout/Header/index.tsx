8a9bddbb5a2b3f66fec6e5db79dbbb32ce39e85b
b0006 <qsefthuken@gmail.com>
2021-10-24 00:16:21 +0700

    fea(header): minot

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 
 import { Container } from '../Container';
 import { Button } from '../../Common/Button';
+import { useMediaBreakpoint } from '../../../hook/useMedia.hook';
+import { BREAKPOINT_SM } from '../../../utils/breakpoints';
 
 import { RightContent } from './RightContent';
 import styles from './Header.module.scss';
 
 const Header: React.FC = () => {
+  const isDesktop = useMediaBreakpoint(BREAKPOINT_SM);
+
   return (
     <header className={styles.header}>
       <Container className={styles.inner}>
         <div className={styles.left}>
-          <Button href="/" icon="home" theme="flat" />
+          <Button href="/" icon="home" theme="flat" text={isDesktop ? 'Главная' : ''} />
         </div>
-        <div className={styles.center}>center</div>
+        <div className={styles.center}>Шахматы онлайн</div>
         <div className={styles.right}>
           <RightContent />
         </div>
