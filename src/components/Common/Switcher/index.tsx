f9a37d5df25ae6f32932541e38e97655c90cb743
b0006 <qsefthuken@gmail.com>
2021-10-24 14:41:58 +0700

    fea(lobby): init game ai settings

--- src/components/Common/Switcher/index.tsx
+++ src/components/Common/Switcher/index.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
+import cn from 'classnames';
 
 import styles from './Switcher.module.scss';
 
-interface Props {
-  value?: boolean;
-  onChange?: (value: boolean) => void;
-}
-
-const Switcher = React.forwardRef((props: Props, ref?: React.LegacyRef<HTMLInputElement>) => {
-  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (typeof props.onChange === 'function') {
-      props.onChange(event.target.checked);
-    }
-  };
-
-  return (
-    <label className={styles.switch}>
-      <input ref={ref} className={styles.input} type="checkbox" checked={props.value} onChange={onInputChange} />
-      <span className={styles.slider} />
-    </label>
-  );
-});
+const Switcher = React.forwardRef(
+  (props: React.InputHTMLAttributes<HTMLInputElement>, ref?: React.LegacyRef<HTMLInputElement>) => {
+    const { className, ...rest } = props;
+    return (
+      <label className={cn(styles.switch, className)}>
+        <input ref={ref} className={styles.input} type="checkbox" {...rest} />
+        <span className={styles.slider} />
+      </label>
+    );
+  }
+);
 
 Switcher.displayName = 'Switcher';
 
--- src/components/Lobby/GameSettingsModal/index.tsx
+++ src/components/Lobby/GameSettingsModal/index.tsx
@@ -1,18 +1,53 @@
 import React from 'react';
+import cn from 'classnames';
+import { useForm } from 'react-hook-form';
 
+import { Button } from '../../Common/Button';
 import { ModalLayout } from '../../Common/ModalLayout';
 import { Switcher } from '../../Common/Switcher';
+
+import styles from './GameSettingsModal.module.scss';
 
 interface Props {
   isVisible: boolean;
   onClose: () => void;
 }
 
+interface FormFields {
+  isAutoPromotion: boolean;
+  isColoredMoves: boolean;
+  isConfirmSteps: boolean;
+  isAudioOn: boolean;
+}
+
 const GameSettingsModal: React.FC<Props> = ({ isVisible, onClose }) => {
+  const { register, handleSubmit } = useForm<FormFields>();
+
+  const onSubmit = async (data: FormFields): Promise<void> => {
+    window.console.log(data);
+  };
+
   return (
-    <ModalLayout isVisible={isVisible} onClose={onClose} overlayClickClose>
-      <form>
-        <Switcher />
+    <ModalLayout classNameInner={styles['modal-inner']} isVisible={isVisible} onClose={onClose} overlayClickClose>
+      <h3 className={styles.title}>Настройка игры</h3>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={cn(styles['switcher-line'], styles.field)}>
+          <span className={styles['switcher-title']}>Авто превращение</span>
+          <Switcher className={styles.switcher} {...register('isAutoPromotion')} />
+        </div>
+        <div className={cn(styles['switcher-line'], styles.field)}>
+          <span className={styles['switcher-title']}>Подсвечивать возможные ходы</span>
+          <Switcher className={styles.switcher} defaultChecked {...register('isColoredMoves')} />
+        </div>
+        <div className={cn(styles['switcher-line'], styles.field)}>
+          <span className={styles['switcher-title']}>Подтверждать каждый ход</span>
+          <Switcher className={styles.switcher} {...register('isConfirmSteps')} />
+        </div>
+        <div className={cn(styles['switcher-line'], styles.field)}>
+          <span className={styles['switcher-title']}>Включить звуки</span>
+          <Switcher className={styles.switcher} {...register('isConfirmSteps')} />
+        </div>
+        <Button type="submit" text="Начать игру" theme="primary" />
       </form>
     </ModalLayout>
   );
