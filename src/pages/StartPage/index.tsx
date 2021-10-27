42e464be40e89bed444039d9dac16f241a6eeb26
b0006 <qsefthuken@gmail.com>
2021-10-27 23:58:34 +0700

    fea(ws): init connection

--- src/pages/StartPage/index.tsx
+++ src/pages/StartPage/index.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { io, Socket } from 'socket.io-client';
 
+import { Button } from '../../components/Common/Button';
 import { MainMenu } from '../../components/Start/MainMenu';
 
 const StartPage: React.FC = () => {
-  return <MainMenu />;
+  const wsRef = React.useRef<Socket<any, any>>();
+
+  React.useEffect(() => {
+    const onConnect = () => {
+      wsRef.current = io('http://localhost:81', {
+        withCredentials: true,
+        transports: ['websocket'],
+      });
+      wsRef.current.on('msgToClient', (message: any) => {
+        console.log('msgToClient', message);
+      });
+    };
+
+    onConnect();
+  }, []);
+
+  const onClick = () => {
+    wsRef.current?.emit('msgToServer', 'teeeeee');
+  };
+
+  return (
+    <div>
+      <Button onClick={onClick} text="Send" />
+      <MainMenu />
+    </div>
+  );
 };
 
 export { StartPage };
